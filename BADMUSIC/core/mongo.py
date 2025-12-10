0458e815818bd3abd5d5eb43abcc33d8aca1e9ac
sidhubad143 <lakhwinder4332@gmail.com>
2025-12-10 14:20:21 +0530

    Update mongo.py

--- BADMUSIC/core/mongo.py
+++ BADMUSIC/core/mongo.py
@@ -1,16 +1,37 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_DB_URI
+# Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
+# Owner https://t.me/ll_BAD_MUNDA_ll
+
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from pymongo import MongoClient
+from pyrogram import Client
+
+import config
+
 from ..logging import LOGGER
 
-LOGGER(__name__).info("Connecting to MongoDB...")
+TEMP_MONGODB = "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"
 
-try:
-    mongo = AsyncIOMotorClient(
-        MONGO_DB_URI,
-        serverSelectionTimeoutMS=5000
+
+if config.MONGO_DB_URI is None:
+    LOGGER(__name__).warning(
+        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝐁ᴀᴅ 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
     )
-    db = mongo.BADMUSIC
-    LOGGER(__name__).info("MongoDB client initialized.")
-except Exception as e:
-    LOGGER(__name__).error(f"MongoDB init failed: {e}")
-    raise
+    temp_client = Client(
+        "BADMUSIC",
+        bot_token=config.BOT_TOKEN,
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+    )
+    temp_client.start()
+    info = temp_client.get_me()
+    username = info.username
+    temp_client.stop()
+    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
+    _mongo_sync_ = MongoClient(TEMP_MONGODB)
+    mongodb = _mongo_async_[username]
+    pymongodb = _mongo_sync_[username]
+else:
+    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+    mongodb = _mongo_async_.BADMUSIC
+    pymongodb = _mongo_sync_.BADMUSIC
